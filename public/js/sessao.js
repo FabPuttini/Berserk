5d5f7674373677ece433f71a4957ce6ff4a6386a
FabPuttini <fabrizzio.puttini@sptech.school>
2025-05-28 13:02:22 -0300

    Login funcionando, sessionStorage verificando login também funcionando

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -18,10 +18,7 @@ function limparSessao() {
 }
 
 // carregamento (loading)
-function aguardar() {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "flex";
-}
+
 /*
 function finalizarAguardar(texto) {
     var divAguardar = document.getElementById("div_aguardar");
---
+++ public/js/sessionStorage.js
@@ -0,0 +1,38 @@
+    let boolean = false;
+    let logado = sessionStorage.CONECTADO;
+    console.log(logado)
+    function exibirSessao() {
+        if (boolean != true) {
+            if (logado == "conectado") {
+                console.log("está logado");
+                sessao.innerHTML = `
+            <div class="block">
+                <ul>
+                    <li><a href="#">Dashboard</a></li>
+                    <li><a href="quiz.html">Quiz</a></li>
+                    <li><a onclick="sair()" style="cursor: pointer;">Logout</a></li>
+                </ul>
+            </div>`
+            }
+            else {
+                console.log(" n está logado");
+                sessao.innerHTML = `
+            <div class="block">
+                <ul>
+                    <li><a href="login.html">Login</a></li>
+                    <li><a href="cadastro.html">Cadastre-se</a></li>
+                </ul>
+            </div>`
+            }
+            
+            boolean = true;
+        }
+        else {
+            sessao.innerHTML = ``
+            boolean = false;
+        }
+    }
+    function sair(){
+        sessionStorage.CONECTADO = "desconectado";
+        window.location = "/login.html";
+    }
